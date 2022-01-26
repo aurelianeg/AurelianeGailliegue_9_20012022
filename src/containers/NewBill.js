55364b970d8c6615e36512c8a6c781602623d98e
Auréliane <aureliane.gailliegue@gmail.com>
2022-01-26 15:01:31 +0100

    Debug hunt on NewBill (as employee and admin)

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -17,28 +17,38 @@ export default class NewBill {
   }
   handleChangeFile = e => {
     e.preventDefault()
-    const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
+    let files = this.document.querySelector(`input[data-testid="file"]`).files
+    const file = files[0]
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
-    const formData = new FormData()
-    const email = JSON.parse(localStorage.getItem("user")).email
-    formData.append('file', file)
-    formData.append('email', email)
+    const fileExt = fileName.split(/\./g)[fileName.split(/\./g).length-1];
+    if (fileExt == "jpg" || fileExt == "jpeg" || fileExt == "png") {
+      const formData = new FormData()
+      const email = JSON.parse(localStorage.getItem("user")).email
+      formData.append('file', file)
+      formData.append('email', email)
 
-    this.store
-      .bills()
-      .create({
-        data: formData,
-        headers: {
-          noContentType: true
-        }
-      })
-      .then(({fileUrl, key}) => {
-        console.log(fileUrl)
-        this.billId = key
-        this.fileUrl = fileUrl
-        this.fileName = fileName
-      }).catch(error => console.error(error))
+      this.store
+        .bills()
+        .create({
+          data: formData,
+          headers: {
+            noContentType: true
+          }
+        })
+        .then(({fileUrl, key}) => {
+          console.log(fileUrl)
+          this.billId = key
+          this.fileUrl = fileUrl
+          this.fileName = fileName
+        }).catch(error => console.error(error))
+    }
+    else {
+      alert("Vous devez choisir un fichier JPG, JPEG ou PNG comme justificatif.")
+      let newFiles = new DataTransfer()
+      files = newFiles.files       // Empty FileList
+      this.document.querySelector(`input[data-testid="file"]`).value = "" // Wrong file name not written on the page
+    }
   }
   handleSubmit = e => {
     e.preventDefault()
